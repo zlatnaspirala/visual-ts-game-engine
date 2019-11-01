313be6836d66e6aeca2161cb05b68a47b00bf27b
zlatnaspirala <zlatnaspirala@gmail.com>
2019-11-02 00:16:28 +0100

    Message box class improved
     - show(contentL string)
     - show welcome message on init
    
       MessageBox is singlton.

--- src/libs/class/messager-box.ts
+++ src/libs/class/messager-box.ts
@@ -3,14 +3,32 @@ import { htmlHeader, byId } from "./system";
 class MessageBox {
 
   private popup: HTMLDivElement;
-  private enabled: boolean = true;
+
+  private showOnInit: boolean = true;
+  private welcomeMessage: string = "This application was created on visual-ts n\ Example: Real time multiplayer `Platformer` zlatnaspirala@gmail.com";
+  private messageBoxContent: HTMLElement;
 
   constructor() {
 
     this.popup = byId("message-box") as HTMLDivElement;
+    this.init();
 
-    if (this.enabled) {
-      this.init();
+  }
+
+   public show (content: string) {
+
+    try {
+      const messageBox = byId("message-box");
+      messageBox.classList.remove("message-box-hide-animation");
+      messageBox.classList.add("message-box-show-animation");
+      this.messageBoxContent = byId("message-box-content");
+      this.messageBoxContent.innerText = content;
+      // Must be sync with css duration value
+      messageBox.style.display = "block";
+    } catch (err) {
+      console.error(err);
+      console.warn("Initialisation depens on async call. If you wanna startup message box \
+       setup showOnInit = true .");
     }
 
   }
@@ -20,7 +38,6 @@ class MessageBox {
     const messageBox = e.currentTarget.parentElement.parentElement;
     messageBox.classList.remove("message-box-show-animation");
     messageBox.classList.add("message-box-hide-animation");
-
     // Must be sync with css duration value
     setTimeout(function() {
       messageBox.style.display = "none";
@@ -40,8 +57,15 @@ class MessageBox {
       }).then(function (html) {
 
         myInstance.popup.innerHTML = html;
-        myInstance.popup.style.display = "block";
+
+        if (myInstance.showOnInit) {
+          myInstance.popup.style.display = "block";
+        } else {
+          myInstance.popup.style.display = "none";
+        }
         myInstance.popup.classList.add("message-box-show-animation");
+        myInstance.messageBoxContent = byId("message-box-content");
+        myInstance.messageBoxContent.innerHTML = myInstance.welcomeMessage;
         byId("message-box-btn").addEventListener("click", myInstance.hide, false);
 
       });
