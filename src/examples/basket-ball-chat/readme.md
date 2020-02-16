0d01fa9bff7ea93555441a7a117934ef0c1fc1d1
zlatnaspirala <zlatnaspirala@gmail.com>
2020-02-16 18:28:14 +0100

    [WIP] Added new game play example
       - new package script run item npm run creator
        Starts python tool.py from submodules.
      - code format

---
+++ src/examples/basket-ball-chat/readme.md
@@ -0,0 +1,25 @@
+
+## Description for Basket Ball Chat application ##
+
+Little different project structure here. Main dependency file is located in examples.
+app.ts & app-platformer-single.ts represent main gamePlay templates.
+Basket Ball Chat is one of different use of this game library.
+
+- Snaped code from multiplayer platformer [0.3.4].
+
+Example structure :
+
+<pre>
+
+├── examples/
+|   ├── basketBallChat/
+|   ├────── audios/
+|   ├────── imgs/
+|   ├────── packs/
+|   |      └── level1.ts
+|   ├────── scripts/
+|   |      └── map.ts
+|   |      └── game-play.ts
+|   |      └── basketBallChat.ts
+
+</pre>
--- webpack.multicompile.config.js
+++ webpack.multicompile.config.js
@@ -7,26 +7,26 @@ const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 let config = {
-    module: {},
+  module: {},
 };
 
 let internalConfig = {
-    createDocumentation: false,
-    stats: "errors-warnings"
+  createDocumentation: false,
+  // stats: "errors-warnings"
 };
 
 let documentationPlugin = new TypedocWebpackPlugin({
-    out: './api-doc',
-    module: 'amd',
-    target: 'es5',
-    exclude: '**/node_modules/**/*.*',
-    experimentalDecorators: true,
-    excludeExternals: true,
-    name: 'sn-theme',
-    mode: 'file',
-    theme: './sn-theme/',
-    includeDeclarations: false,
-    ignoreCompilerErrors: true,
+  out: './api-doc',
+  module: 'amd',
+  target: 'es5',
+  exclude: '**/node_modules/**/*.*',
+  experimentalDecorators: true,
+  excludeExternals: true,
+  name: 'sn-theme',
+  mode: 'file',
+  theme: './sn-theme/',
+  includeDeclarations: false,
+  ignoreCompilerErrors: true,
 });
 
 /**
@@ -34,8 +34,9 @@ let documentationPlugin = new TypedocWebpackPlugin({
  */
 
 const rootBuildPath = "/build";
-const appMultiplayerPlatform = "/multiplayer"
-const appSingleplayerPlatform = "/singleplayer"
+const appMultiplayerPlatform = "/multiplayer";
+const appSingleplayerPlatform = "/singleplayer";
+const appMultiBasketBallChatPlatform = "/basket-ball-chat";
 
 const resolveExtensions = {
   extensions: [".js", ".ts", ".tsx", ".json"]
@@ -44,30 +45,30 @@ const resolveExtensions = {
 const roles = [
   { test: /\.tsx?$/, loader: "ts-loader" },
   {
-    test: /\.(jpg|png)$/, loader: "file-loader", options: {
-      name: '[name].[ext]',
-      outputPath: "./imgs"
-    }
+  test: /\.(jpg|png)$/, loader: "file-loader", options: {
+    name: '[name].[ext]',
+    outputPath: "./imgs"
+  }
   },
   { test: /\.css$/, loader: "style-loader!css-loader" },
   {
-    test: /\.(ico)$/,
-    use: {
-      loader: 'file-loader',
-      options: {
-        name: '[name].[ext]',
-        outputPath: '/styles'
-      }
-    }
-  },
-  {
-    test: /\.(mp4|ogg)$/,
-    // include: __dirname + "/src/examples/platformer-single-player/audios",
+  test: /\.(ico)$/,
+  use: {
     loader: 'file-loader',
     options: {
-        name: '[name].[ext]',
-        outputPath: "/audios"
+    name: '[name].[ext]',
+    outputPath: '/styles'
     }
+  }
+  },
+  {
+  test: /\.(mp4|ogg)$/,
+  // include: __dirname + "/src/examples/platformer-single-player/audios",
+  loader: 'file-loader',
+  options: {
+    name: '[name].[ext]',
+    outputPath: "/audios"
+  }
   },
   // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
   { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
@@ -81,83 +82,83 @@ const roles = [
 
 let webPackModuleMultiPlayerSolution = {
 
-    mode: "development",
-    watch: true,
-    stats: internalConfig.stats,
-    entry: ["./src/app.ts"],
-    output: {
-        filename: "visualjs2.js",
-        path: __dirname + rootBuildPath + appMultiplayerPlatform,
-    },
+  mode: "development",
+  watch: true,
+  stats: internalConfig.stats,
+  entry: ["./src/app.ts"],
+  output: {
+    filename: "visualjs2.js",
+    path: __dirname + rootBuildPath + appMultiplayerPlatform,
+  },
 
-    devtool: "none",
+  devtool: "none",
 
-    resolve: resolveExtensions,
+  resolve: resolveExtensions,
 
-    module: {
-        rules: roles
-    },
+  module: {
+    rules: roles
+  },
 
-    plugins: [
-        // Make sure that the plugin is after any plugins that add images
-        new CleanWebpackPlugin(['build'], { /*exclude:  ['index.html']*/ }),
-        new HtmlWebpackPlugin({
-            filename: 'app.html',
-            template: 'src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/register.html',
-            template: 'src/html-components/register.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/login.html',
-            template: 'src/html-components/login.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/user-profile.html',
-            template: 'src/html-components/user-profile.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/store.html',
-            template: 'src/html-components/store.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/games-list.html',
-            template: 'src/html-components/games-list.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/video-conference.html',
-            template: 'src/html-components/video-conference.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/broadcaster.html',
-            template: 'src/html-components/broadcaster.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/message-box.html',
-            template: 'src/html-components/message-box.html'
-        }),
-        new ScriptExtHtmlWebpackPlugin({
-            defaultAttribute: 'defer'
-        }),
-        new ExtractTextPlugin("src/style/styles.css"),
-        new CopyWebpackPlugin([
-            { from: 'src/style/broadcaster.css', to: 'styles/broadcaster.css' },
-            { from: 'src/style/getHTMLMediaElement.css', to: 'styles/getHTMLMediaElement.css' },
-            { from: './src/libs/addons/hacker-timer/hack-timer.js', to: 'externals/hack-timer.js'},
-            { from: './src/libs/addons/drag/drag.ts', to: 'externals/drag.ts' },
-            { from: './src/libs/addons/hacker-timer/hack-timer-worker.js', to: 'externals/hack-timer-worker.js' },
-            { from: './src/manifest.web', to: 'manifest.web' },
-            { from: './src/libs/addons/cache/cacheInit.ts', to: 'externals/cacheInit.ts' },
-            { from: './src/libs/addons/cache/worker.js', to: 'worker.js' },
-            { from: './src/libs/addons/cache/offline.html', to: 'offline.html' },
-            { from: './src/libs/addons/webrtc-adapter/adapter.js', to: 'externals/adapter.js' },
-            { from: "./src/examples/platformer/ui/player-board.html", to: "templates/ui/player-board.html"},
-            { from: "./src/examples/platformer/ui/select-player.html", to: "templates/ui/select-player.html"},
-        ], { debug: 'warn' }),
-        // { debug: 'info' } make trace
+  plugins: [
+    // Make sure that the plugin is after any plugins that add images
+    new CleanWebpackPlugin(['build'], { /*exclude:  ['index.html']*/ }),
+    new HtmlWebpackPlugin({
+      filename: 'app.html',
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/register.html',
+      template: 'src/html-components/register.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/login.html',
+      template: 'src/html-components/login.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/user-profile.html',
+      template: 'src/html-components/user-profile.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/store.html',
+      template: 'src/html-components/store.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/games-list.html',
+      template: 'src/html-components/games-list.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/video-conference.html',
+      template: 'src/html-components/video-conference.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/broadcaster.html',
+      template: 'src/html-components/broadcaster.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/message-box.html',
+      template: 'src/html-components/message-box.html'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+    new ExtractTextPlugin("src/style/styles.css"),
+    new CopyWebpackPlugin([
+      { from: 'src/style/broadcaster.css', to: 'styles/broadcaster.css' },
+      { from: 'src/style/getHTMLMediaElement.css', to: 'styles/getHTMLMediaElement.css' },
+      { from: './src/libs/addons/hacker-timer/hack-timer.js', to: 'externals/hack-timer.js'},
+      { from: './src/libs/addons/drag/drag.ts', to: 'externals/drag.ts' },
+      { from: './src/libs/addons/hacker-timer/hack-timer-worker.js', to: 'externals/hack-timer-worker.js' },
+      { from: './src/manifest.web', to: 'manifest.web' },
+      { from: './src/libs/addons/cache/cacheInit.ts', to: 'externals/cacheInit.ts' },
+      { from: './src/libs/addons/cache/worker.js', to: 'worker.js' },
+      { from: './src/libs/addons/cache/offline.html', to: 'offline.html' },
+      { from: './src/libs/addons/webrtc-adapter/adapter.js', to: 'externals/adapter.js' },
+      { from: "./src/examples/platformer/ui/player-board.html", to: "templates/ui/player-board.html"},
+      { from: "./src/examples/platformer/ui/select-player.html", to: "templates/ui/select-player.html"},
+    ], { debug: 'warn' }),
+    // { debug: 'info' } make trace
 
-    ],
+  ],
 
 };
 
@@ -167,58 +168,147 @@ let webPackModuleMultiPlayerSolution = {
 
 let webPackModuleSingleSimpleSolution = {
 
-    mode: "development",
-    watch: true,
-    stats: internalConfig.stats,
-    entry: ["./src/app-platformer-single.ts"],
-    output: {
-        filename: "visualjs2.js",
-        path: __dirname + rootBuildPath + appSingleplayerPlatform,
-    },
+  mode: "development",
+  watch: true,
+  stats: internalConfig.stats,
+  entry: ["./src/app-platformer-single.ts"],
+  output: {
+    filename: "visualjs2.js",
+    path: __dirname + rootBuildPath + appSingleplayerPlatform,
+  },
 
-    devtool: "none",
+  devtool: "none",
 
-    resolve: resolveExtensions,
+  resolve: resolveExtensions,
 
-    module: {
-        rules: roles
-    },
+  module: {
+    rules: roles
+  },
 
-    plugins: [
-        // Make sure that the plugin is after any plugins that add images
-        new CleanWebpackPlugin(['build'], { /*exclude:  ['index.html']*/ }),
-        new HtmlWebpackPlugin({
-            filename: 'app.html',
-            template: 'src/index.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'templates/message-box.html',
-            template: 'src/html-components/message-box.html'
-        }),
-        new ScriptExtHtmlWebpackPlugin({
-            defaultAttribute: 'defer'
-        }),
-        new ExtractTextPlugin("src/style/styles.css"),
-        new CopyWebpackPlugin([
-            { from: './src/libs/addons/hacker-timer/hack-timer.js', to: 'externals/hack-timer.js'},
-            { from: './src/libs/addons/drag/drag.ts', to: 'externals/drag.ts' },
-            { from: './src/libs/addons/hacker-timer/hack-timer-worker.js', to: 'externals/hack-timer-worker.js' },
-            { from: './src/manifest.web', to: 'manifest.web' },
-            { from: './src/libs/addons/cache/cacheInit.ts', to: 'externals/cacheInit.ts' },
-            { from: './src/libs/addons/cache/worker.js', to: 'worker.js' },
-            { from: './src/libs/addons/cache/offline.html', to: 'offline.html' },
-        ], { debug: 'warn' })
-        // { debug: 'info' } make trace
+  plugins: [
+    // Make sure that the plugin is after any plugins that add images
+    new CleanWebpackPlugin(['build'], { /*exclude:  ['index.html']*/ }),
+    new HtmlWebpackPlugin({
+      filename: 'app.html',
+      template: 'src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/message-box.html',
+      template: 'src/html-components/message-box.html'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+    new ExtractTextPlugin("src/style/styles.css"),
+    new CopyWebpackPlugin([
+      { from: './src/libs/addons/hacker-timer/hack-timer.js', to: 'externals/hack-timer.js'},
+      { from: './src/libs/addons/drag/drag.ts', to: 'externals/drag.ts' },
+      { from: './src/libs/addons/hacker-timer/hack-timer-worker.js', to: 'externals/hack-timer-worker.js' },
+      { from: './src/manifest.web', to: 'manifest.web' },
+      { from: './src/libs/addons/cache/cacheInit.ts', to: 'externals/cacheInit.ts' },
+      { from: './src/libs/addons/cache/worker.js', to: 'worker.js' },
+      { from: './src/libs/addons/cache/offline.html', to: 'offline.html' },
+    ], { debug: 'warn' })
+    // { debug: 'info' } make trace
 
-    ],
+  ],
+
+};
+
+ /**
+  * WebPacks objects
+  *
+  * 1) WebPack for multiplayer solution
+  */
+
+let webPackModuleMultiChatBasketBall = {
+
+  mode: "development",
+  watch: true,
+  stats: internalConfig.stats,
+  entry: ["./src/examples/basket-ball-chat/appBasketBallChat.ts"],
+  output: {
+    filename: "visualjs2.js",
+    path: __dirname + rootBuildPath + appMultiBasketBallChatPlatform,
+  },
+
+  devtool: "none",
+
+  resolve: resolveExtensions,
+
+  module: {
+    rules: roles
+  },
+
+  plugins: [
+    // Make sure that the plugin is after any plugins that add images
+    new CleanWebpackPlugin(['build'], { /*exclude:  ['index.html']*/ }),
+    new HtmlWebpackPlugin({
+      filename: 'app.html',
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/register.html',
+      template: './src/html-components/register.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/login.html',
+      template: './src/html-components/login.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/user-profile.html',
+      template: './src/html-components/user-profile.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/store.html',
+      template: './src/html-components/store.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/games-list.html',
+      template: './src/html-components/games-list.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/video-conference.html',
+      template: './src/html-components/video-conference.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/broadcaster.html',
+      template: './src/html-components/broadcaster.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'templates/message-box.html',
+      template: './src/html-components/message-box.html'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    }),
+    new ExtractTextPlugin("./src/style/styles.css"),
+    new CopyWebpackPlugin([
+      { from: './src/style/broadcaster.css', to: 'styles/broadcaster.css' },
+      { from: './src/style/getHTMLMediaElement.css', to: 'styles/getHTMLMediaElement.css' },
+      { from: './src/libs/addons/hacker-timer/hack-timer.js', to: 'externals/hack-timer.js'},
+      { from: './src/libs/addons/drag/drag.ts', to: 'externals/drag.ts' },
+      { from: './src/libs/addons/hacker-timer/hack-timer-worker.js', to: 'externals/hack-timer-worker.js' },
+      { from: './src/manifest.web', to: 'manifest.web' },
+      { from: './src/libs/addons/cache/cacheInit.ts', to: 'externals/cacheInit.ts' },
+      { from: './src/libs/addons/cache/worker.js', to: 'worker.js' },
+      { from: './src/libs/addons/cache/offline.html', to: 'offline.html' },
+      { from: './src/libs/addons/webrtc-adapter/adapter.js', to: 'externals/adapter.js' },
+      { from: "./src/examples/platformer/ui/player-board.html", to: "templates/ui/player-board.html"},
+      { from: "./src/examples/platformer/ui/select-player.html", to: "templates/ui/select-player.html"},
+    ], { debug: 'warn' }),
+    // { debug: 'info' } make trace
+
+  ],
 
 };
 
 if (internalConfig.createDocumentation == true) {
-    webPackModule.plugins.push(documentationPlugin);
+  webPackModule.plugins.push(documentationPlugin);
 }
 
 module.exports = [
   webPackModuleSingleSimpleSolution,
-  webPackModuleMultiPlayerSolution
+  webPackModuleMultiPlayerSolution,
+  webPackModuleMultiChatBasketBall
 ];
