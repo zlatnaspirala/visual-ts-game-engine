06d1f27ad4dc979770dbb717e9ea80ee2ff32ad2
Nidza <zlatnaspirala@gmail.com>
2020-10-01 19:01:31 +0200

    CRYPTO ERROR at the moment [WIP]

--- server/common/crypto.js
+++ server/common/crypto.js
@@ -6,14 +6,15 @@ class CryptoHandler {
     this.crypto = require('crypto');
     this.algorithm = 'aes-256-cbc';
     this.key = this.crypto.randomBytes(32);
-    this.iv = this.crypto.randomBytes(16);
+    // let iv = Buffer.from(textParts.shift(), 'hex');
+    this.iv = "nikola";
 
   }
 
   encrypt(text) {
 
     const password = text;
-    const cipher = this.crypto.createCipher('aes128', "a password");
+    const cipher = this.crypto.createDecipheriv('aes128', "a password");
     var encrypted = cipher.update(password, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return encrypted;
@@ -23,12 +24,29 @@ class CryptoHandler {
   decrypt(text) {
 
     const encrypt_password = text;
-    const decipher = this.crypto.createDecipher('aes128','a password');
+    const decipher = this.crypto.createDecipheriv('aes128','a password');
     var decrypted = decipher.update(encrypt_password,'hex', 'utf8');
     decrypted += decipher.final('utf8');
     return decrypted;
 
   }
+
+  /**
+   *
+   * const crypto = require('crypto')
+const {algorithm, iv} = require('config').crypto
+
+module.exports = {
+	decrypt({cipherText, key}) {
+		const decipher = crypto.createDecipheriv(algorithm, key, iv)
+		return decipher.update(cipherText, 'hex', 'utf8') + decipher.final('utf8')
+	}
+	, encrypt({text, key}) {
+		const cipher = crypto.createCipheriv(algorithm, key, iv)
+		return cipher.update(text, 'utf8', 'hex') + cipher.final('hex')
+	}
+}
+   */
 
 }
 module.exports = CryptoHandler;
