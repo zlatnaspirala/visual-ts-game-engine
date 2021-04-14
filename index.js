488683dde852d876bfdf8b5bbd1076e556102501
zlatnaspirala <zlatnaspirala@gmail.com>
2021-04-14 17:54:50 +0200

    Test npm implementation

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-/**
- * Test proper implementation with npm service 
- * TEST
- */
-
-var visualTs = require("./src/app")
-exports.visualTs = visualTs
---
+++ index.ts
@@ -0,0 +1,29 @@
+'use strict'
+
+import { byId } from "./src/libs/class/system";
+import SpriteTextureComponent from "./src/libs/class/visual-methods/sprite-animation";
+import * as Matter from "matter-js";
+import BotBehavior from "./src/libs/class/bot-behavior";
+import TextComponent from "./src/libs/class/visual-methods/text";
+import TextureComponent from "./src/libs/class/visual-methods/texture";
+import { DEFAULT_GAMEPLAY_ROLES, DEFAULT_RENDER_BOUNDS } from "./src/libs/defaults";
+import Starter from "./src/libs/starter";
+import { worldElement } from "./src/libs/types/global";
+
+// import GameMap from "./map";
+// import Level1 from "./packs/level1";
+// import Platformer from "./Platformer";
+
+/**
+ * @description
+ * Test proper implementation with npm service
+ * Test
+ */
+const visualTs = {
+  Matter,
+  SpriteTextureComponent,
+  TextureComponent
+}
+
+export { visualTs }
+
