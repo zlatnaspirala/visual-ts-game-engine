d7f5ba46ba3607ad8e147fb23fc5cbe375c3c2d4
zlatnaspirala <zlatnaspirala@gmail.com>
2024-02-13 17:36:00 +0100

    Update server.js

--- server/rtc/server.js
+++ server/rtc/server.js
@@ -32,13 +32,13 @@ if (serverConfig.ownHosting == true) {
   var https = require("https");
   var hostingHTTP = express();
 
-  if (serverConfig.serverMode === "dev" || serverConfig.serverMode === "mongodb.net") {
+  if (serverConfig.serverMode === "dev" || serverConfig.serverMode === "mongodb.net-dev") {
     options = {
       key: fs.readFileSync(serverConfig.certPathSelf.pKeyPath),
       cert: fs.readFileSync(serverConfig.certPathSelf.pCertPath),
       ca: fs.readFileSync(serverConfig.certPathSelf.pCBPath),
     };
-  } else if (serverConfig.serverMode === "prod") {
+  } else if (serverConfig.serverMode === "prod" || serverConfig.serverMode === "mongodb.net") {
     options = {
       key: fs.readFileSync(serverConfig.certPathProd.pKeyPath),
       cert: fs.readFileSync(serverConfig.certPathProd.pCertPath),
